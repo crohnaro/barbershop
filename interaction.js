24ee478222f6838972aa77434af43846caada98f
crohnaro <viniciosalfa@hotmail.com>
2022-02-22 09:46:49 -0300

    Erro na branch, corrigindo o push adicionando iteratividades js para o botão

---
+++ interaction.js
@@ -0,0 +1,7 @@
+const btnMobile = document.getElementById('btn-mobile');
+function toogleMenu(){
+    const menuMobile = document.getElementsById('menuMobile');
+    nav.classList.toogleMenu('active');
+
+}
+btnMobile.addEventListener('click', toogleMenu);
